f0f7a48044fd594ce0787ed50dcff11795e6a8a2
sumnous <sumnous.ting@gmail.com>
2014-03-17 16:26:33 +0800

    fix reverseKGroup.py

--- Linear_List/Linked_List/reverseKGroup.py
+++ Linear_List/Linked_List/reverseKGroup.py
@@ -14,11 +14,11 @@ Created on 2014-03-17
 #For k = 2, you should return: 2->1->4->3->5
 #For k = 3, you should return: 3->2->1->4->5
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#Definition for singly-linked list.
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
 class Solution:
     # @param head, a ListNode
@@ -27,21 +27,47 @@ class Solution:
     def reverseKGroup(self, head, k):
         if head == None or head.next == None or k < 2:
             return head
-        next_group = head
-        for i in range(k):
-            if next_group != None:
-                next_group = next_group.next
+        dummy = ListNode(-1)
+        dummy.next = head
+        pre = dummy
+        while head != None:
+            for i in range(1, k+1):
+                if head.next == None:
+                    break
+                head = head.next
+            if i == k:
+                cur = pre.next
+                head = pre.next.next
+                for i in range(1, k):
+                    tmp = head.next
+                    cur.next = head.next
+                    head.next = pre.next
+                    pre.next = head
+                    head = tmp
+                pre = cur
+                cur.next = head
             else:
-                return head
-        new_next_group = self.reverseKGroup(next_group, k)
-        prev = None
-        cur = head
-        while cur != next_group:
-            probe = cur.next
-            cur.next = prev
-            prev = cur
-            cur = cur.next
-        if cur == prev:
-            return prev
-        else:
-            return new_next_group
+                break
+        return dummy.next
+def generateLinkedList(array):
+    if len(array) == 0:
+        return None
+    head = ListNode(array[0])
+    curr = head
+    for i in range(1, len(array)):
+        node = ListNode(array[i])
+        curr.next = node
+        curr = node
+    return head
+
+def printLinkedList(curr):
+    result = []
+    while curr != None:
+        result.append(curr.val)
+        curr = curr.next
+    print(result)
+
+if __name__ == '__main__':
+    s = Solution()
+    linked = s.reverseKGroup(generateLinkedList([1,2,3,4]),2)
+    printLinkedList(linked)
