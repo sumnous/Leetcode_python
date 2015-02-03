216e90771b668e9cafa8c6346b1ab8d26afc48c0
sumnous <sumnous.ting@gmail.com>
2015-02-03 18:00:56 +0800

    add 151

--- reverseWords.py
+++ reverseWords.py
@@ -1,36 +1,23 @@
-#186
-#Reverse Words in a String II
-#
-#Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
-#
-#The input string does not contain leading or trailing spaces and the words are always separated by a single space.
+#151
+#Reverse Words in a String
+#Given an input string, reverse the string word by word.
 #
 #For example,
 #Given s = "the sky is blue",
 #return "blue is sky the".
-#
-#Could you do it in-place without allocating extra space?
 
 class Solution:
-    # @param s, a list of 1 length strings, e.g., s = ['h','e','l','l','o']
-    # @return nothing
+    # @param s, a string
+    # @return a string
     def reverseWords(self, s):
+        s = s.split(' ')
+        s = [x for x in s if x != '']
         n = len(s)
         for i in range(n/2):
-            s[i],s[n-i-1] = s[n-i-1],s[i]
-        i = 0; num = 0
-        while i < n:
-            if s[i] != ' ':
-                i += 1; num += 1
-            else:
-                for j in range(num/2):
-                    s[i-num+j],s[i-j-1] = s[i-j-1],s[i-num+j]
-                i += 1; num = 0
-        for j in range(num/2):
-            s[i-num+j],s[i-j-1] = s[i-j-1],s[i-num+j]
+            s[i], s[n-i-1] = s[n-i-1], s[i]
+        return ' '.join(s)
 
 if __name__ == '__main__':
     s = Solution()
-    str = "the sky is blue"
-    s.reverseWords(list(str))
-
+    str = "  a  b  "
+    print s.reverseWords(str)
